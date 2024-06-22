c1f04f3953f54dd5b2e353056d2e8ab7315d223c
Arsalan Azmi <mrlethalazo@gmail.com>
2024-06-22 16:06:42 +0100

    Add qibla endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -10,17 +10,35 @@ routes.get("/", async (req, res) => {
   res.send("Hayya Alassalah API working OK");
 });
 
+async function getPrayerTimings(address: string, date?: string, school: number = 1, method?: string) {
+  try {
+    const apiUrl = `http://api.aladhan.com/v1/timingsByAddress/${date}?address=${address}&school=${school}&method=${method || ""}`;
+    const response = await axios.get(apiUrl);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching prayer timings:", error.message);
+    throw new Error("Error fetching prayer timings");
+  }
+}
+
+routes.get("/qibla", async (req, res) => {
+  try {
+    const { address } = req.query;
+    const prayerTimings = await getPrayerTimings(address.toString());
+    const { latitude, longitude } = prayerTimings.data.meta;
+    const qiblaApiUrl = `http://api.aladhan.com/v1/qibla/${latitude}/${longitude}`;
+    const qiblaResponse = await axios.get(qiblaApiUrl);
+    res.json(qiblaResponse.data);
+  } catch (error) {
+    console.error("Error fetching Qibla direction:", error.message);
+    res.status(500).send("Error fetching Qibla direction");
+  }
+});
+
 routes.get("/timings/", async (req, res) => {
   try {
-    const { date, address, school = 1, method } = req.query;
-
-    // Construct the Aladhan API URL
-    const apiUrl = `http://api.aladhan.com/v1/timingsByAddress/${date}?address=${address}&school=${school}&method=${method || ""}`;
-
-    // Make a request to the Aladhan API
-    const response = await axios.get(apiUrl);
-
-    // Send the prayer timings data back to the client
+    const { address, date, school = 1, method } = req.query;
+    const response = await getPrayerTimings(address.toString(), date ? date.toString() : null, parseInt(school.toString()), method ? method.toString() : null);
     res.json(response.data);
   } catch (error) {
     console.error("Error fetching prayer timings:", error.message);
